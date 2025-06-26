877aaf6d687f2269329446460a1b6c37df620a89
SohitBennett <E22CSEU1409@bennett.edu.in>
2025-06-26 18:58:08 +0530

    removed api key & tried .env

--- constants/index.ts
+++ constants/index.ts
@@ -1,5 +1,10 @@
 // constants/index.ts
-export const OPEN_WEATHER_API_KEY = "9e8da42dbb0946cb2194522d2a407a13";
+export const OPEN_WEATHER_API_KEY = "";
 export const OPEN_WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather";
 
 
+// constants/index.ts
+// import { OPEN_WEATHER_API_KEY as KEY } from '@env';
+
+// export const OPEN_WEATHER_API_KEY = KEY;
+// export const OPEN_WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather";
---
+++ env.d.ts
@@ -0,0 +1,4 @@
+// env.d.ts
+declare module '@env' {
+  export const OPEN_WEATHER_API_KEY: string;
+}
